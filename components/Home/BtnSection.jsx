8108574134cfc3afe6591d320e3ea8fa073ec29b
Dan Garro <dobetterwithdan@gmail.com>
2023-12-26 16:24:41 -0500

    created btn logos for anna site

--- components/Home/BtnSection.jsx
+++ components/Home/BtnSection.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 import { FaInstagram } from 'react-icons/fa'
 import { FaFacebook } from 'react-icons/fa'
+import Oil from '../../assets/OP.png'
+import WoodB from '../../assets/WB.png'
+import Tattoo from '../../assets/T.png'
+import Contact from '../../assets/contact.png'
+import Info from '../../assets/Info.png'
 
 const BtnSection = () => {
   return (
     <div className='flex flex-row justify-around rounded my-2 font-sans text-xs bg-gradient-to-b from-slate-100 via-sky-100 to-white p-2.5'>
-      <button className='box-border h-14 p-2 w-20 rounded-r-lg bg-slate-500 hover:bg-amber-800 hover:font-bold text-white'>
-        Wood Burning
+      <button className='box-border h-16 w-16 rounded mx-1 bg-slate-500 hover:bg-amber-800'>
+        <Image className='rounded' src={WoodB} alt='Wood-Burning-Btn' />
       </button>
-      <button className='box-border h-14 w-20 p-2 rounded-lg mx-1 bg-slate-500 hover:bg-cyan-800 hover:font-bold text-white'>
-        Oil Painting
+      <button className='box-border h-16 w-16 rounded mx-1 bg-slate-500 hover:bg-cyan-800 hover:font-bold text-white'>
+        <Image className='rounded' src={Oil} alt='oil-painting-btn' />
       </button>
-      <button className='box-border h-14 w-20 px-2 py-2 rounded-lg mx-1 bg-slate-500 hover:bg-slate-900 hover:font-bold text-white'>
-        Tattoos
+      <button className='box-border h-16 w-16 rounded mx-1 bg-slate-500 hover:bg-slate-900 hover:font-bold text-white'>
+        <Image className='rounded' src={Tattoo} alt='tattoo-btn' />
       </button>
       <div className='flex justify-between my-1.5'>
         <Link
@@ -21,21 +27,25 @@ const BtnSection = () => {
           target='_blank'
           className='hover:font-semibold hover:text-white'
         >
-          <FaFacebook className='text-5xl pr-2 hover:text-sky-700' />
+          <FaFacebook className='text-4xl pr-2 hover:text-sky-700' />
         </Link>
         <Link
           href='https://www.instagram.com/'
           target='_blank'
           className='hover:font-semibold hover:text-white'
         >
-          <FaInstagram className='text-5xl pl-2 hover:text-sky-700' />
+          <FaInstagram className='text-4xl pl-2 hover:text-sky-700' />
         </Link>
       </div>
-      <button className='box-border h-14 w-20 px-2 py-2 rounded-lg mx-1 bg-slate-500 hover:bg-slate-800 hover:font-bold text-white'>
-        <Link href='/about'>About</Link>
+      <button className='box-border h-16 w-16 rounded mx-1 bg-slate-500 hover:bg-slate-800 hover:font-bold text-white'>
+        <Link href='/about'>
+          <Image className='rounded' src={Info} alt='info-logo' />
+        </Link>
       </button>
-      <button className='box-border h-14 w-20 px-2 py-2 mx-1 rounded-l-lg bg-slate-500 hover:bg-slate-800 hover:font-bold text-white'>
-        <Link href='/contact'>Contact</Link>
+      <button className='box-border h-16 w-16 mx-1 rounded bg-slate-500 hover:bg-slate-800 hover:font-bold text-white'>
+        <Link href='/contact'>
+          <Image className='rounded' src={Contact} alt='contact-logo' />
+        </Link>
       </button>
     </div>
   )
