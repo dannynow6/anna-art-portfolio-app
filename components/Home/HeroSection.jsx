1698d2819d8370eb41f4e5427548d22286bf5bf0
Dan Garro <dobetterwithdan@gmail.com>
2024-01-01 12:30:07 -0500

    styled hero section for home page - created reusable nav section to add to all heros

--- components/Home/HeroSection.jsx
+++ components/Home/HeroSection.jsx
@@ -2,15 +2,17 @@
 
 // import Image from 'next/image'
 import React from 'react'
+import NavSection from './NavSection'
 
 const HeroSection = () => {
   return (
     <div className='hero-section'>
-      <div className='flex flex-row w-full justify-center'>
-        <h2 className='text-slate-50 font-sans text-6xl z-10'>
-          Anna Bissinger
+      <div className='flex w-full justify-center'>
+        <h2 className='text-slate-50 font-sans py-2 px-1.5 text-6xl mt-6 z-10 rounded-lg max-h-28 bg-slate-50 bg-opacity-15 sm:mt-4 md:mt-6 lg:mt-8 xl:mt-10 sm:text-3xl md:text-4xl lg:text-5xl xl:text-8xl'>
+          Anna Saveria
         </h2>
       </div>
+      <NavSection />
     </div>
   )
 }
--- components/Home/NavSection.jsx
+++ components/Home/NavSection.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { usePathname } from 'next/navigation'
+//import { usePathname } from 'next/navigation'
 import { FaInstagram } from 'react-icons/fa'
 import { FaFacebook } from 'react-icons/fa'
-import { TBMenu2 } from 'react-icons/tb'
+import { IoMenuOutline } from 'react-icons/io5'
+import Contact from '../../assets/contact.png'
 
 /*
     Create reusable Nav Section to be included in Hero section for each page of 
@@ -16,11 +17,28 @@ import { TBMenu2 } from 'react-icons/tb'
 */
 
 const NavSection = () => {
-  const pathname = usePathname()
+  //const pathname = usePathname()
 
   return (
     <>
-      <div className='flex justify-between'></div>
+      <nav className='rounded-lg h-12 bg-slate-50 bg-opacity-15 text-white w-full'>
+        <div className='flex justify-between mx-3'>
+          <IoMenuOutline className='text-4xl mt-1.5 hover:text-cyan-600' />
+          <div className='flex justify-around'>
+            <Link href='https://www.facebook.com/' target='_blank'>
+              <FaFacebook className='social-btn' />
+            </Link>
+            <Link href='https://www.instagram.com/' target='_blank'>
+              <FaInstagram className='social-btn' />
+            </Link>
+          </div>
+          <button className='nav-btn'>
+            <Link href='/contact'>
+              <Image className='rounded' src={Contact} alt='contact-logo' />
+            </Link>
+          </button>
+        </div>
+      </nav>
     </>
   )
 }
